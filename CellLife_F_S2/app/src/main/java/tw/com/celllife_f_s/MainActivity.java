5d1ec21f9ef4383e4fe47aab0cf790f51fbdc2eb
CS-FFZ <316585692@qq.com>
2017-06-03 10:05:50 +0800

    CheckInput
    检查第一个界面输入长宽能否正确的输入进入APP

--- CellLife_F_S2/app/src/main/java/tw/com/celllife_f_s/MainActivity.java
+++ CellLife_F_S2/app/src/main/java/tw/com/celllife_f_s/MainActivity.java
@@ -1,13 +1,28 @@
 package tw.com.celllife_f_s;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new LifeView());
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.commit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int width = Integer.parseInt(((EditText)findViewById(R.id.editWidth)).getText().toString());
+                int height = Integer.parseInt(((EditText)findViewById(R.id.editHeight)).getText().toString());
+                Log.i(TAG, "onClick: width "+ width );
+                Log.i(TAG, "onClick: height "+ height );
+            }
+        });
     }
 }
